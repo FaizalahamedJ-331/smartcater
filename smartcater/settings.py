06c6845618557433f22318d183536b3a5b217d69
Faizal Ahamed <faizalahamedj331@gmail.com>
2026-05-09 21:46:51 +0530

    updated for debugging

--- smartcater/settings.py
+++ smartcater/settings.py
@@ -18,11 +18,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = [
-    'Faizal001.pythonanywhere.com',
-    '127.0.0.1',
-    'localhost',
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
