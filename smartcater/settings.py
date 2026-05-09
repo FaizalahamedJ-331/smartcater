03105e209ab48839195d4d09b94dcddf612e4e61
Faizal Ahamed <faizalahamedj331@gmail.com>
2026-05-09 21:53:48 +0530

    Fix Vercel allowed hosts

--- smartcater/settings.py
+++ smartcater/settings.py
@@ -16,10 +16,18 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "smartcater-three.vercel.app",
+    ".vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://smartcater-three.vercel.app",
+]
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
